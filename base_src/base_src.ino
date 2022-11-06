14269c81bdce768212b95ec82621f1b0ebb25f88
Josh Booth <boothjmail@gmail.com>
2022-11-05 20:03:30 -0700

    working controller

--- base_src/base_src.ino
+++ base_src/base_src.ino
@@ -1,4 +1,8 @@
 #include <AccelStepper.h>
+#include <Wire.h>
+
+void dataRcv(int numBytes);
+
 
 // Define pin connections
 const int altDirPin = 5;
@@ -17,17 +21,35 @@ const int AzStepPin = 3;
 AccelStepper AltStepper(motorInterfaceType, altStepPin, altDirPin);
 AccelStepper AzStepper(motorInterfaceType, AzStepPin, AzDirPin);
 
+typedef struct {
+  bool up = 0;
+  bool down = 0;
+  bool left = 0;
+  bool right = 0;
+  bool speed_led_toggle = 0;
+  bool speed_led = 0; // start in high speed mode (to indicate initial power bc of LED)
+  int16_t x = 0; // value from 0-4 indicating speed
+  int16_t y = 0; // value from 0-4 indicating speed
+} control_t;
+
+volatile static control_t controls;
+
 void setup() {
+  Wire.begin(0x08);           // join I2C bus as Slave with address 0x08
+  // event handler initializations
+  Wire.onReceive(dataRcv);    // register an event handler for received data
 
   // enable stepper motor drivers
   pinMode(8, OUTPUT);
   digitalWrite(8, LOW);
 
-//  pinMode(altDirPin, OUTPUT);
-//  digitalWrite(altDirPin, HIGH);
-//
-//  pinMode(altStepPin, OUTPUT);
-//  digitalWrite(altStepPin, HIGH);
+  //  pinMode(altDirPin, OUTPUT);
+  //  digitalWrite(altDirPin, HIGH);
+  //
+  //  pinMode(altStepPin, OUTPUT);
+  //  digitalWrite(altStepPin, HIGH);
+  Serial.begin(9600);
+  //Serial2.begin(9600, SERIAL_7O1);
 
   // set the maximum speed, acceleration factor,
   // initial speed and the target position
@@ -39,10 +61,50 @@ void setup() {
 }
 
 void loop() {
-  // Change direction once the motor reaches target position
-  if (myStepper.distanceToGo() == 0) 
-    myStepper.moveTo(-myStepper.currentPosition());
+  //Serial.print("here2");
 
-  // Move the motor one step
-  myStepper.run();
+  Serial.print("Up: ");
+  Serial.print(controls.up);
+  Serial.print(", Down: ");
+  Serial.print(controls.down);
+  Serial.print(", Left: ");
+  Serial.print(controls.left);
+  Serial.print(", Right: ");
+  Serial.print(controls.right);
+  Serial.print(", Y: ");
+  Serial.print(controls.y);
+  Serial.print(", X: ");
+  Serial.print(controls.x);
+  Serial.println();
+  //  // Change direction once the motor reaches target position
+  //  if (myStepper.distanceToGo() == 0)
+  //    myStepper.moveTo(-myStepper.currentPosition());
+  //
+  //  // Move the motor one step
+  //  myStepper.run();
+}
+
+//received data handler function
+void dataRcv(int numBytes) {
+  static uint8_t tempX;
+  while (Wire.available()) {
+    for (int i = 0; i < numBytes; i++) { // should read 4 bytes
+      byte inData = Wire.read(); // throw out first value
+      switch (i) {
+        case 0:
+          controls.left = inData & 1;
+          controls.right = inData & (1 << 1);
+          controls.up = inData & (1 << 2);
+          controls.down = inData & (1 << 3);
+          controls.speed_led = inData & (1 << 4);
+          break;
+        case 1:
+          controls.x = inData;
+          break;
+        case 2:
+          controls.y = inData;
+          break;
+      }
+    }
+  }
 }
--- remote_src/remote_src.ino
+++ remote_src/remote_src.ino
@@ -1,14 +1,33 @@
-#define UP                PIN_PB3
-#define DOWN              PIN_PB4
-#define LEFT              PIN_PB6
-#define RIGHT             PIN_PB1
-#define X                 PIN_PD5
-#define Y                 PIN_PD3
-#define SPEED_LED         PIN_PA1
-#define SPEED_LED_TOGGLE  PIN_PB0
+#define UP                PIN_PD2
+#define DOWN              PIN_PB1
+#define LEFT              PIN_PB2
+#define RIGHT             PIN_PD5
+#define X                 PIN_PC3
+#define Y                 PIN_PC2
+#define SPEED_LED         PIN_PB0
+#define SPEED_LED_TOGGLE  PIN_PD7
+
+// I2C master
+#include <Wire.h>
+
+#define SPEED_LED_START_VAL 1
 
 
-#define MAX_ANALOG_VAL 
+typedef struct {
+  bool up = 0;
+  bool down = 0;
+  bool left = 0;
+  bool right = 0;
+  bool speed_led_toggle = 0;
+  bool speed_led = SPEED_LED_START_VAL; // start in high speed mode (to indicate initial power bc of LED)
+  bool yIsNeg = 0;
+  bool xIsNeg = 0;
+  byte x = 0; // value from 0-4 indicating speed
+  byte y = 0; // value from 0-4 indicating speed
+  byte to_send[4];
+} control_t;
+
+static control_t controls;
 
 void setup() {
   // input buttons
@@ -23,23 +42,17 @@ void setup() {
   // joystick speed indicator
   pinMode(SPEED_LED, OUTPUT);
 
-  // Setup UART
-  Serial.begin(9600, SERIAL_8O1); // enable odd parity
+  // populate divs
+//  divs[0] = ZERO_VAL;
+//  for(int i = 1; i < DIVS; i++){
+//    divs[i] = (MAX_ANALOG_VAL/(DIVS-1))*i;
+//  }
+
+  // Setup I2C
+  Wire.begin();
+
+  digitalWrite(SPEED_LED, SPEED_LED_START_VAL);
 }
-
-
-typedef struct {
-  bool up = 0;
-  bool down = 0;
-  bool left = 0;
-  bool right = 0;
-  bool speed_led_toggle = 0;
-  bool speed_led = 1; // start in high speed mode (to indicate initial power bc of LED)
-  int x = 0; // value from 0-4 indicating speed
-  int y = 0; // value from 0-4 indicating speed
-} control_t
-
-static control_t controls;
 
 void loop() {
   // read buttons
@@ -50,16 +63,26 @@ void loop() {
   controls.speed_led_toggle = digitalRead(SPEED_LED_TOGGLE);
   
   // set speed LED
-  if(controls.speed_led_toggle) speed_led = !speed_led;
+  if(!controls.speed_led_toggle){
+    while(!digitalRead(SPEED_LED_TOGGLE)); // wait for toggle to be released
+    controls.speed_led = !controls.speed_led;
+    digitalWrite(SPEED_LED, controls.speed_led);
+  }
   
-  // read joystick X
-  int tempX = analogRead(X);
-  
-  // read joystick Y
-  int tempY = analogRead(Y);
-  
-  // combine into hex
+  // read joystick X (
+  controls.x = analogRead(X) / 4;
 
-  // send hex
-  Serial.write(0x);
+  // read joystick Y
+  controls.y = analogRead(Y) / 4;
+
+  // combine binary
+  controls.to_send[0] = (controls.speed_led << 4) | (controls.down << 3) | (controls.up << 2) | (controls.right << 1) | (controls.left) ;
+  controls.to_send[1] = controls.x;
+  controls.to_send[2] = controls.y;
+
+  // send binary
+  Wire.beginTransmission(0x08);
+  Wire.write(controls.to_send, 3);
+  Wire.endTransmission();
+
 }
